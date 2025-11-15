f5971e78ee4fbf432aa48ca15a45ab84f2a67566
VGMil <vgmiltonisaac@gmail.com>
2025-11-15 11:49:16 -0500

    ✨feat(ui): implementar sistema de componentes UI (#3)
    - Crear un sistema de renderizado de componentes con funciones auxiliares.
    - Implementar el primer componente 'InputText' con su lógica y estilos.
    - Establecer una estructura de carpetas escalable para futuros componentes.
    - Añadir estilos globales y una paleta de colores base.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,13 @@
 <?php
-// --- Test de phpinfo() ---
-echo "<h1>¡Hola desde el proyecto Reactify en Docker!</h1>";
-echo "<h2>Información de PHP:</h2>";
-phpinfo();
+require_once __DIR__ . '/functions.php';
+$host = 'db'; // Nombre del servicio de la base de datos en docker-compose.yml
+$dbname = 'reactifyDB'; // Nombre de la BD definida en el .env
+$user = 'reactify'; // Usuario definido en el .env
+$pass = 'reactify'; // Contraseña definida en el .env
+$charset = 'utf8mb4';
 
-// --- Test de Conexión a la Base de Datos ---
-echo "<hr>";
-echo "<h2>Test de Conexión a MySQL:</h2>";
-
- $host = 'db'; // Nombre del servicio de la base de datos en docker-compose.yml
- $dbname = 'reactifyDB'; // Nombre de la BD definida en el .env
- $user = 'reactify'; // Usuario definido en el .env
- $pass = 'reactify'; // Contraseña definida en el .env
- $charset = 'utf8mb4';
-
- $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
- $options = [
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+$options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
@@ -31,3 +23,28 @@ try {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./global.css">
+    <?php render_css('ui/InputText'); ?>
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    echo "<h2>Test de Conexión a MySQL:</h2>";
+    render_component('ui/InputText', [
+        'name' => 'email',
+        'placeholder' => 'tu@email.com',
+        'value' => '',
+        'required' => 'required',
+    ]);
+    ?>
+</body>
+
+</html>
