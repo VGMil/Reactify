c13b222327203de0125c07590293b7ce7fa3b6d8
VGMil <vgmiltonisaac@gmail.com>
2025-11-15 15:01:05 -0500

    ♻️ refactor(auth): mejorar estructura del formulario de login - Crear el estilo '.form-field' para envolver cada par etiqueta/campo. - Refactorizar 'auth/Login.php' para usar la nueva estructura 'form-field'. - Integrar el componente 'ui/Text' para todos los elementos de texto (títulos, etiquetas) para garantizar consistencia tipográfica.

--- src/auth/Login.php
+++ src/auth/Login.php
@@ -14,37 +14,69 @@ require_once __DIR__ . '/../functions.php';
     <?php render_css('ui/InputText'); ?>
     <?php render_css('ui/Button'); ?>
     <?php render_css('ui/Card'); ?>
+    <?php render_css('ui/Text'); ?>
 
 </head>
 
 <body>
-    
+
     <?php
     render_component('ui/Card', [
-        'children' => function() {
-            ?>
-            <form action="auth.php" method="POST" class="form">
+        'children' => function () {
+    ?>
+        <form action="auth.php" method="POST" class="form">
+
+
+            <?php render_component('ui/Text', [
+                'tag' => 'h1',
+                'children' => 'Iniciar Sesión'
+            ]); ?>
+            
+            <?php render_component('ui/Text', [
+                'tag' => 'h2',
+                'children' => 'Ingresa tus crendeciales para continuar',
+                'size' => 'small',
+                'variant' => 'muted'
+            ]); ?>
+
+            <!-- email -->
+            <div class="form-field">
+                <?php render_component('ui/Text', [
+                    'tag' => 'h3',
+                    'children' => 'Email'
+                ]); ?>
+
                 <?php render_component('ui/InputText', [
                     'name' => 'correo',
                     'type' => 'email',
                     'placeholder' => 'Correo Electrónico',
                     'required' => true
                 ]); ?>
+            </div>
 
+            <!-- password -->
+            <div class="form-field">
+                <?php render_component('ui/Text', [
+                    'tag' => 'h3',
+                    'children' => 'Password'
+                ]); ?>
                 <?php render_component('ui/InputText', [
                     'name' => 'password',
                     'type' => 'password',
                     'placeholder' => 'Contraseña',
                     'required' => true
                 ]); ?>
+            </div>
 
-                <?php render_component('ui/Button', [
-                    'type' => 'submit',
-                    'children' => 'Entrar',
-                    'variant' => 'submit'
-                ]); ?>
-            </form>
-            <?php
+
+            <?php render_component('ui/Button', [
+                'type' => 'submit',
+                'children' => 'Entrar',
+                'variant' => 'submit'
+            ]); ?>
+
+        </form>
+    <?php
         }
     ]);
     ?>
