4fb8b66c86dd0bfbfabf0f33bc0133654f63325c
VGMil <vgmiltonisaac@gmail.com>
2025-11-15 21:22:51 -0500

    ✨ feat(dashboard): Agregar la pagina de `dashboard`. - En `index` se incorpora la ruta de `dashboard`. - `dashboard` esta estructurada en grilla para futuras implementaciones.

--- src/index.php
+++ src/index.php
@@ -4,16 +4,20 @@ require_once './lib/Route.php';
 use Lib\Route;
 
 Route::get('/', function () {
-    $login_file_path = __DIR__ . '/auth/Login/Login.php';
-    include $login_file_path;
+    $file_path = __DIR__ . '/auth/Login/Login.php';
+    include $file_path;
 });
 Route::get('/login',function(){
-    $login_file_path = __DIR__ . '/auth/Login/Login.php';
-    include $login_file_path;
+    $file_path = __DIR__ . '/auth/Login/Login.php';
+    include $file_path;
 });
 Route::get('/register',function(){
-    $login_file_path = __DIR__ . '/auth/Register/Register.php';
-    include $login_file_path;
+    $file_path = __DIR__ . '/auth/Register/Register.php';
+    include $file_path;
+});
+Route::get('/dashboard',function(){
+    $file_path = __DIR__ . '/home/dashboard/dashboard.php';
+    include $file_path;
 });
 
 Route::dispatch();
